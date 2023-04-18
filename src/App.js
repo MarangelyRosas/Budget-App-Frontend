d7d6725558cd2729250686dfc66b1637df7b127d
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-18 12:59:14 -0400

    Imoported BrowserRouter and started creating necessary routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <NavBar />
+        <main>
+         <Routes> 
+             {/* <Route path="/" element={<Home />} /> 
+            <Route path="/transaction" element={<Index />} /> 
+          <Route path="/transaction/new" element={<New />} />  */}
+           {/* <Route path="/bookmarks/:index" element={<Show />} /> */}
+           {/* <Route path="/bookmarks/:index/edit" element={<Edit />} /> */}
+           {/* <Route path="*" element={<FourOFour />} /> */} 
+        </Routes> 
+        </main>
+      </Router>
+    </div> 
+ );
 }
 
 export default App;
