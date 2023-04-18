3b020339e8a8d1ac902623a016fd090f311aeb39
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-18 19:24:51 -0400

    Importing Pages and adding Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import NavBar from './Components/NavBar';
+import Home from './Pages/Home';
+import Index from './Pages/Index';
+import New from './Pages/New';
+import Show from './Pages/Show';
+import Edit from './Pages/Edit';
+import FourOFour from './Pages/FourOFour';
+import TransactionDetails from './Components/TransactionDetails';
 
 function App() {
   return (
@@ -9,13 +17,13 @@ function App() {
       <Router>
         <NavBar />
         <main>
-         <Routes> 
-             {/* <Route path="/" element={<Home />} /> 
-            <Route path="/transaction" element={<Index />} /> 
-          <Route path="/transaction/new" element={<New />} />  */}
-           {/* <Route path="/bookmarks/:index" element={<Show />} /> */}
-           {/* <Route path="/bookmarks/:index/edit" element={<Edit />} /> */}
-           {/* <Route path="*" element={<FourOFour />} /> */} 
+          <Routes> 
+            <Route path="/" element={<Home />} /> 
+            <Route path="/transactions" element={<Index />} /> 
+            <Route path="/transaction/new" element={<New />} />  
+            <Route path="/transactions/:id" element={<Show />} /> 
+            {/* <Route path="/transactions/:index/edit" element={<Edit />} /> */}
+            {/* <Route path="*" element={<FourOFour />} />  */}
         </Routes> 
         </main>
       </Router>
