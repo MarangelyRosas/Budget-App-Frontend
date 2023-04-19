a0cd138c3523ad25b7be78dde677b1e0fb4454de
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-19 02:04:23 -0400

    Did my best with 3 days of no sleep!

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -4,29 +4,34 @@ import Transaction from "./Transaction";
 
 function Transactions() {
   const [transactions, setTransactions] = useState([]);
-
+  let total = 0;
   useEffect(() => {
-    axios
-    .get(`${process.env.REACT_APP_API_URL}/transactions`)
-    .then((res) => {
+    axios.get(`${process.env.REACT_APP_API_URL}/transactions`).then((res) => {
       setTransactions(res.data);
-      console.log(res.data)
-    });
-}, []);
+    })}, []);
 
+    transactions.map(e => e.amount).map(e2 => total += e2)
   return (
     <div >
       <section>
         <table>
           <thead>
             <tr>
-              <th>Date</th>
-              <th>Item Name</th>
-              <th>Amount</th>
+             
+             
+              <th>Bank Account Total: ${total}</th>
+              {/* <th>{total}</th> */}
             </tr>
           </thead>
           <tbody>
+
+              <tr>
+                 <th>Date</th>
+                 <th>Item Name</th>
+                 <th>Amount</th>
+              </tr>
             {transactions.map((trans, index) => {
+                
               return <Transaction key={index} trans={trans} />;
             })}
           </tbody>
